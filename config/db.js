973f6da521aa349fa9f42faaa4598a904359c41d
Dimas Neto <dimas_fcn@outlook.com>
2023-01-23 09:27:13 -0300

    models

--- config/db.js
+++ config/db.js
@@ -1,3 +1,21 @@
 const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
 
-// connection
+const conn = async () => {
+  try {
+    const dbConn = await mongoosr.connect(
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.moqfuou.mongodb.net/?retryWrites=true&w=majority`
+    );
+
+    console.log("Conectou ao banco");
+
+    return dbConn;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+conn();
+
+module.exports = conn;
