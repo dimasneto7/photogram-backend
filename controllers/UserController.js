ca003a7338ad9115d59781590ac7598855fdac98
Dimas Neto <dimas_fcn@outlook.com>
2023-01-23 16:24:25 -0300

    authentication

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -50,6 +50,41 @@ const register = async (req, res) => {
   });
 };
 
+// Sign user in
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  // check if user exists
+  if (!user) {
+    res.status(404).json({ errors: ["Usuário não encontrado."] });
+    return;
+  }
+
+  // check if password matches
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(422).json({ errors: ["Senha inválida."] });
+    return;
+  }
+
+  // return user with token
+  res.status(201).json({
+    _id: user._id,
+    profileImage: user.profileImage,
+    token: generateToken(user._id),
+  });
+};
+
+// Get current logged in user
+const getCurrentUser = async (req, res) => {
+  const user = req.user;
+
+  res.status(200).json(user);
+};
+
 module.exports = {
   register,
+  login,
+  getCurrentUser,
 };
