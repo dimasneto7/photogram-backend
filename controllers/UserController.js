017ecff38f20f218671a50acd14b7d221686dcfd
Dimas Neto <dimas_fcn@outlook.com>
2023-01-24 14:52:30 -0300

    updateUser

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -50,6 +50,13 @@ const register = async (req, res) => {
   });
 };
 
+// Get current logged in user
+const getCurrentUser = async (req, res) => {
+  const user = req.user;
+
+  res.status(200).json(user);
+};
+
 // Sign user in
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -76,15 +83,73 @@ const login = async (req, res) => {
   });
 };
 
-// Get current logged in user
-const getCurrentUser = async (req, res) => {
-  const user = req.user;
+// Update an user
+const update = async (req, res) => {
+  const { name, password, bio } = req.body;
+
+  let profileImage = null;
+
+  if (req.file) {
+    profileImage = req.file.filename;
+  }
+
+  const reqUser = req.user;
+
+  const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select(
+    "-password"
+  );
+
+  if (name) {
+    user.name = name;
+  }
+
+  if (password) {
+    // Generate password hash
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    user.password = passwordHash;
+  }
+
+  if (profileImage) {
+    user.profileImage = profileImage;
+  }
+
+  if (bio) {
+    user.bio = bio;
+  }
+
+  await user.save();
 
   res.status(200).json(user);
+};
+
+// Get user by id
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(mongoose.Types.ObjectId(id)).select(
+      "-password"
+    );
+
+    // check if user exists
+    if (!user) {
+      res.status(404).json({ errors: ["Usuário não encontrado."] });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(422).json({ errors: ["Usuário não encontrado."] });
+    return;
+  }
 };
 
 module.exports = {
   register,
   login,
   getCurrentUser,
+  update,
+  getUserById,
 };
